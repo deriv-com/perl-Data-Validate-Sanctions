8d0f7ef3588277af5a83acae8716c72d2bff6e4e
Mehdi Zarei <mehdi@firstsource.tech>
2021-09-14 06:30:29 +0000

    wip

--- lib/Data/Validate/Sanctions/Fetcher.pm
+++ lib/Data/Validate/Sanctions/Fetcher.pm
@@ -427,10 +427,14 @@ sub run {
     my $result = {};
     my $ua     = Mojo::UserAgent->new;
     $ua->connect_timeout(15);
+    $ua->inactivity_timeout(60);
 
     my $config = config(%args);
+    my $retries = $args{retries} // 3;
 
     foreach my $id (sort keys %$config) {
+        my $retry_counter = 0;
+        
         my $source = $config->{$id};
         try {
             die "Url is empty for $id" unless $source->{url};
@@ -438,12 +442,13 @@ sub run {
             my $raw_data;
 
             if ($source->{url} =~ m/^file:\/\/(.*)$/) {
-                open my $fh, '<', "$1" or die "Can't open $id file $1 $!";
-                $raw_data = do { local $/; <$fh> };
-                close $fh;
+                $raw_data = _entries_from_file();
             } else {
-                die "File not downloaded for $id" if $ua->get($source->{url})->result->is_error;
-                $raw_data = $ua->get($source->{url})->result->body;
+                $raw_data = _entries_from_remote_src({
+                    id => $id,
+                    source => $source->{url},
+                    retries => 3
+                });
             }
 
             my $data = $source->{parser}->($raw_data);
@@ -454,11 +459,61 @@ sub run {
                 print "Source $id: $count entries fetched \n" if $args{verbose};
             }
         } catch {
-            warn "$id list update failed: $@";
+            warn "$id list update failed because: $@";
         }
     }
 
     return $result;
+}
+
+=head2 _entries_from_file
+
+Get the sanction entries from a file locally
+
+=cut
+
+sub _entries_from_file {
+    my $entries;
+    
+    open my $fh, '<', "$1" or die "Can't open $id file $1 $!";
+    $entries = do { local $/; <$fh> };
+    close $fh;
+
+    return $entries;
+}
+
+=head2 _entries_from_remote_src
+
+Get the sanction entries from a remote source includes retry mechanism
+
+=cut
+
+sub _entries_from_remote_src {
+    my ($args) = @_;
+
+    my ($id, $src_url, $retries) = @{$args}{qw/ id source retries /};
+    $retries //= 3;
+
+    my $entries;
+    my $error_log = 'Unknown Error';
+
+    my $retry_counter = 0;
+    while($retry_counter < $retries){
+        $retry_counter++;
+
+        try {
+            my $resp = $ua->get($src_url);
+
+            die "File not downloaded for $id" if $resp->result->is_error;
+            $entries = $resp->result->body;
+
+            last;
+        } catch {
+            $error_log = $@;
+        }
+    }
+
+    return $entries or die "An error occurred while fetching data from '$src_url' due to $error_log";
 }
 
 1;
