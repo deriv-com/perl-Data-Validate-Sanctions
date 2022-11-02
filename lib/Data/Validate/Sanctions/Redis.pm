fad04a3a8f32bb1bf2f026e52c439586d4068487
ragheb-deriv <ragheb.dargham@deriv.com>
2022-11-02 12:50:11 +0000

    bug fix [ci]

--- lib/Data/Validate/Sanctions/Redis.pm
+++ lib/Data/Validate/Sanctions/Redis.pm
@@ -123,11 +123,11 @@ __END__
 Data::Validate::Sanctions::Redis - An extension of L<Data::Validate::Sanctions::Redis> that stores sanction data in redis.
 
 =head1 SYNOPSIS
+
     ## no critic
+    use Data::Validate::Sanctions::Redis;
     
-    use Data::Validate::Sanctions::Redis; 
-    
-    my $validator = Data::Validate::Sanctions::Redis->new(connection => $redis_read); 
+    my $validator = Data::Validate::Sanctions::Redis->new(connection => $redis_read);
     
     # to validate clients by their name
     print 'BAD' if $validator->is_sanctioned("$last_name $first_name");
@@ -137,7 +137,6 @@ Data::Validate::Sanctions::Redis - An extension of L<Data::Validate::Sanctions::
     # to update the sanction dataset (needs redis write access)
     my $validator = Data::Validate::Sanctions::Redis->new(connection => $redis_write); ## no critic
     $validator->update_data(eu_token => $token);
-
 
     # create object from the parent (factory) class
     my $validator = Data::Validate::Sanctions->new(storage => 'redis', connection => $redis_write);
